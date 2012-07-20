bbfcd0a53f7c4d38d27ff2084bdb56fbe2661f02
miketrumpis <mtrumpis@gmail.com>
2012-07-20 11:54:57 -0400

    auto-tuned scale for knn-graphs

--- ecoglib/graph/knn_graph.py
+++ ecoglib/graph/knn_graph.py
@@ -1,17 +1,79 @@
 import numpy as np
 import scipy.sparse as sparse
 
-def knn_graph(neighbors, dists=None):
+def gauss_affinity(d_sq, sig_sq):
+    """
+    The Gaussian affinity function is defined as
+
+    a(i,j) = exp{-d(i,j)**2/(2*sig**2)}
+
+    Parameters
+    ----------
+
+    d_sq: ndarray
+      A flat array of all d(i,j)**2 values to compute
+    sig_sq: float or array
+      The characteristic distance scale, or possibly an adaptive scale
+      for to each (i,j) pair, listed in the same order as d_sq
+    """
+    return np.exp(-d_sq/(2*sig_sq))
+
+
+def knn_graph(neighbors, dists=None, scale=1.0, auto_scale=0):
+    """
+    Build a sparse adjacency matrix of a k-nearest neighbor graph.
+    By common definition, this graph includes all vertices
+
+    { (i,j) : i \in N_k(j) OR j \in N_k(i) }
+
+    To construct the mutual k-nearest neighbor graph, see the
+    "mutual" parameter below.
+
+    Self-loops are not restricted, and will be encoded in the graph
+    if the first column of neighbors is identical to the index order.
+
+    Parameters
+    ----------
+
+    neighbors: ndarray (n_vertices, k)
+      The vertices are ordered by index, and the values of each
+      row indicate the index of k-nearest neighboring vertices.
+
+    dists: ndarray (n_vertices, k)
+      The optional distances to the k-nearest neighbors of each vertex.
+      If given, the graph is weighted. Otherwise, it is a connectivity
+      graph (binary edge weights).
+
+    scale: float (Default 1.0)
+      The characteristic distance scale used in the Gaussian affinity
+      kernel. Alternatively use auto_scale for adaptive scale.
+
+    auto_scale: int (Default 0)
+      If distances are given, then automatically tune the characteristic
+      distance scale between vertices based on their respective ith
+      nearest neighbors. The given value of auto_scale is used as "i".
+
+    mutual: boolean (Default False)
+      Construct a mutual k-nearest neighbors graph, which includes
+      all vertices (i,j) such that i is a neighbor of j AND j is
+      a neighbor of i. (NOT YET IMPLEMENTED)
+
+    """
+
     Nk = set()
-    # if no self-loops, then zip on (neighbors[:,1:], dists[:,1:])
+    n_vert = neighbors.shape[0]
+    if auto_scale:
+        scale = np.empty(n_vert)
     if dists is None:
-        for i, i_nb in enumerate(neighbors[:,1:]):
+        for i, i_nb in enumerate(neighbors):
             Nk.update( ( (i,j) for j in i_nb ) )
             Nk.update( ( (j,i) for j in i_nb ) )
     else:
-        for i, (i_nb, i_dist) in enumerate(zip(neighbors[:,1:], dists[:,1:])):
+        for i, (i_nb, i_dist) in enumerate(zip(neighbors, dists)):
             Nk.update( ( (i, j, w) for j, w, in zip(i_nb, i_dist) ) )
             Nk.update( ( (j, i, w) for j, w, in zip(i_nb, i_dist) ) )
+            if auto_scale:
+                scale[i] = i_dist[auto_scale]
 
     ## Nk_sym = set( ( (i,j,d) for (i,j,d) in Nk if (j,i,d) in Nk ) )
     ## Nk = Nk_sym
@@ -21,8 +83,13 @@ def knn_graph(neighbors, dists=None):
         csr_dat = np.ones(len(Nk))
     else:
         csr_ind = np.array( [ [i,j,w] for i,j,w in Nk ] )
-        csr_dat = np.exp(-csr_ind[:,-1]**2)
-        csr_ind = csr_ind[:,:2]
-    npts = len(neighbors)
-    W = sparse.csr_matrix((csr_dat, csr_ind.T), shape=(npts, npts))
+        csr_dist = csr_ind[:,2]
+        csr_ind = csr_ind[:,:2].astype('i')
+        if auto_scale:
+            sig_sq = np.take(scale, csr_ind)
+            sig_sq = sig_sq[:,0] * sig_sq[:,1]
+        else:
+            sig_sq = scale**2
+        csr_dat = gauss_affinity(csr_dist**2, sig_sq)
+    W = sparse.csr_matrix((csr_dat, csr_ind.T), shape=(n_vert, n_vert))
     return W
