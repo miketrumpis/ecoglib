68fb01e9490ceade9c1bd3d775e11f30757508ba
miketrumpis <mtrumpis@gmail.com>
2013-11-04 14:24:58 -0500

    batch mode for TDMS conversion

--- cmdline/tdms_to_hdf5.py
+++ cmdline/tdms_to_hdf5.py
@@ -4,6 +4,7 @@ import tables
 import nptdms
 import tempfile
 import os, sys
+from glob import glob
 import contextlib
 
 def tdms_to_hdf5(
@@ -138,10 +139,12 @@ if __name__ == '__main__':
 
     prs = argparse.ArgumentParser(description='Convert TDMS to HDF5')
     prs.add_argument(
-        'tdms_file', help='path to the TDMS file', type=str
+        'tdms_file', nargs = '+',
+        help='path to the TDMS file', type=str
         )
     prs.add_argument(
-        'h5_file', help='name of the HDF5 file to create', type=str
+        'h5_file', nargs = '+',
+        help='name of the HDF5 file to create', type=str
         )
     prs.add_argument(
         '-p', '--permutation', type=str, default='',
@@ -155,11 +158,39 @@ if __name__ == '__main__':
         '-z', '--compression', type=int, default=0,
         help='use zlib level # compression in HDF5'
         )
+    prs.add_argument(
+        '-b', '--batch', help='Batch process all matching files',
+        action='store_true'
+        )
 
     args = prs.parse_args()
 
-    tdms_to_hdf5(
-        args.tdms_file, args.h5_file, chan_map=args.permutation,
-        memmap=args.memmap, compression_level=args.compression
-        )
+    if args.batch:
+        hp = args.h5_file[0]
+        if not os.path.isdir(hp):
+            (hp, pf) = os.path.split(hp)
+        else:
+            pf = ''
+        if len(args.tdms_file) > 1:
+            # shell has globbed *.tdms
+            all_tdms = args.tdms_file
+        else:
+            tdms = args.tdms_file[0]
+            (tp, _) = os.path.split(tdms)
+            all_tdms = glob(os.path.join(tp, '*.tdms'))
+        all_h5 = list()
+        for tdms in all_tdms:
+            (_, tf) = os.path.split(tdms)
+            (tf, ext) = os.path.splitext(tf)
+            all_h5.append( os.path.join(hp, pf+tf+'.h5') )
+    else:
+        all_tdms = args.tdms_file
+        all_h5 = args.h5_file
+
+    for tf, hf in zip(all_tdms, all_h5):
+        print tf, '\t', hf
+        tdms_to_hdf5(
+            tf, hf, chan_map=args.permutation, memmap=args.memmap,
+            compression_level=args.compression
+            )
 
