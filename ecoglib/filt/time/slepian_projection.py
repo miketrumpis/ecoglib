a730b0605e23ec0abdf2fb96080f70bad6a9841f
miketrumpis <mtrumpis@gmail.com>
2016-02-12 17:24:50 -0500

    implemented filtering through Slepian taper projection

---
+++ ecoglib/filt/time/slepian_projection.py
@@ -0,0 +1,41 @@
+import numpy as np
+from nitime.algorithms import dpss_windows
+from sandbox.array_split import split_at
+
+# This still crashes on Mac due to VecLib not playing nicely with forks
+# @split_at() 
+def slepian_projection(data, BW, Fs, Kmax=None, w0=0, baseband=False):
+    shp = data.shape
+    if len(shp) < 2:
+        data = np.atleast_2d(data)
+    if len(shp) > 2:
+        data = data.reshape( shp[0] * shp[1], -1 )
+    nchan, npts = data.shape
+    # find NW which is also TW
+    T = npts / Fs
+    # round to the nearest multiple of 1/2
+    TW = round( 2 * T * BW ) / 2.0
+    K = 2 * TW
+    if Kmax is not None:
+        K = min(Kmax, K)
+    dpss, _ = dpss_windows(npts, TW, K)
+    if w0 == 0:
+        # shortcut for lowpass only
+        w = data.dot( dpss.T )
+        # w is (nchan x K)
+        # expand Slepians as (nchan x npts)
+        bp =  w.dot( dpss )
+    else:
+        t = np.arange(npts)
+        dpss_pf = np.exp(2j * np.pi * w0 * t / Fs) * dpss
+        dpss_nf = np.exp(-2j * np.pi * w0 * t / Fs) * dpss
+        nrm = np.sqrt( ( dpss_nf * dpss_nf.conj() ).sum(-1) )
+        dpss_nf /= nrm[:, None]
+        wp = data.dot( dpss_pf.conj().T )
+        wn = data.dot( dpss_nf.conj().T )
+        if baseband:
+            bp = ( wp.dot( dpss ) + wn.dot( dpss ) ).real
+        else:
+            bp = ( wp.dot( dpss_pf ) + wn.dot( dpss_nf ) ).real
+    return bp.reshape(shp)
+
---
+++ ecoglib/filt/time/tests/tests_slepian_projection.py
@@ -0,0 +1,51 @@
+from nose.tools import assert_true
+ 
+from numpy.testing import assert_almost_equal
+import numpy as np
+
+from ecoglib.filt.time.slepian_projection import slepian_projection
+
+def gen_sig(am=False, w0=80, bw=30, nfreqs=10):
+    freqs = (np.random.rand(10) - 0.5) * bw
+    if w0 and not am:
+        freqs += w0
+    amps = np.random.randn(10)
+    phs = np.random.rand(10) * 2 * np.pi
+    tx = np.arange(2000) * 2 * np.pi / 1000.
+    narrowband = amps[:, None] * np.cos(freqs[:,None]*tx + phs[:,None])
+    narrowband = narrowband.sum(0)
+    if am:
+        modulated = narrowband * np.cos(tx * w0)
+        return modulated, narrowband
+    return narrowband
+
+def test_baseband_recon():
+    modulated, baseband = gen_sig(am=True)
+    recon = slepian_projection(modulated, 50, 1000.0, w0=80, baseband=True)
+    # I guess < 0.1% error is good -- sometimes boundary effects, so
+    # check within interior of signal
+    err = recon[150:-150] - baseband[150:-150]
+    rel_error = np.sum(err**2) / np.sum(baseband[150:-150]**2)
+    assert_true( rel_error < 1e-3 )
+
+def test_narrowband_recon():
+    nb = gen_sig()
+    nb_est = slepian_projection(nb, 50, 1000.0, w0=80)
+    # I guess < 0.1% error is good -- sometimes boundary effects, so
+    # check within interior of signal
+    err = nb_est[150:-150] - nb[150:-150]
+    rel_error = np.sum(err**2) / np.sum(nb[150:-150]**2)
+    assert_true( rel_error < 1e-3 )
+
+def test_bandpass_power():
+    sg = np.random.randn(2000)
+    # This bandpass window is from 150 to 250 (and -150 to -250),
+    # Given the hypothetical bandwidth of 500 Hz, it should
+    # take up ~ 20% of the signal power (which is pretty much unit-valued)
+    sg_bp = slepian_projection(sg, 50, 1000, w0=200)
+    assert_almost_equal(sg_bp.var(), 0.2, decimal=1)
+    # This bandpass window is from -50 to 50, 
+    # Given the hypothetical bandwidth of 500 Hz, it should
+    # take up ~ 10% of the signal power (which is pretty much unit-valued)
+    sg_bp = slepian_projection(sg, 50, 1000, w0=0)
+    assert_almost_equal(sg_bp.var(), 0.1, decimal=1)
