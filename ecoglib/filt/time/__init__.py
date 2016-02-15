d3e6ae84f7b6b06603a47f5a8ba4468c9777a1bc
miketrumpis <mtrumpis@gmail.com>
2016-02-15 08:16:28 -0500

    access to package functions

--- ecoglib/filt/time/__init__.py
+++ ecoglib/filt/time/__init__.py
@@ -1,3 +1,4 @@
-from blocked_filter import *
-from design import *
-from proc import *
+from .blocked_filter import *
+from .design import *
+from .proc import *
+from .slepian_projection import slepian_projection
