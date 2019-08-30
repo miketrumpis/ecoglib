606f2461463f35407c9a37af23909bf18d4cddaa
miketrumpis <mtrumpis@gmail.com>
2019-08-30 16:04:34 -0700

    delete more files

--- ecoglib/mproc.py
+++
@@ -1,9 +0,0 @@
-"""Safely set numexpr.set_num_threads(1) before attempting multiprocessing"""
-
-import numexpr
-numexpr.set_num_threads(1)
-
-from multiprocessing import *
-import multiprocessing.sharedctypes
-sharedctypes = multiprocessing.sharedctypes
-
