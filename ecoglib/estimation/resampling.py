9f8944b48789d50781996f2b4bb7141b7e2751cf
miketrumpis <mtrumpis@gmail.com>
2020-07-06 15:07:29 -0700

    resampling updates: need spawn tests in windo

--- ecoglib/estimation/resampling.py
+++ ecoglib/estimation/resampling.py
@@ -7,7 +7,8 @@ from itertools import combinations
 from contextlib import closing, ExitStack
 import ecogdata.parallel.mproc as mp
 from ecogdata.util import get_default_args
-from ecogdata.parallel.sharedmem import SharedmemManager
+from ecogdata.parallel.sharedmem import SharedmemManager, shared_copy
+from ecogdata.parallel.array_split import timestamp
 
 
 __all__ = ['random_combinations', 'Jackknife', 'Bootstrap']
@@ -20,44 +21,173 @@ def random_combinations(iterable, r, n):
     pulls = set()
     pool = tuple(iterable)
     L = len(pool)
+    # TODO: this is VERY SLOW
     while len(pulls) < n:
         indices = sorted(random.sample(range(L), r))
         pulls.add(tuple(pool[i] for i in indices))
     return pulls
 
 
-def _init_pool(pool_args):
-    """stick a dictionary of variable names / values into the global namespace of the process worker"""
-    for kw in pool_args.keys():
-        globals()[kw] = pool_args[kw]
-
-
-def _resampler(index):
-    """Light array sampler using shared memory.
-
-    The multiprocess Pool is initialized so that these variables are in
-    global memory:
-
-    * shm_arrays: list of arrays to be resampled on the given axis
-    * axis
-    * estimator
-    * e_args
-    * e_kwargs
-
+def resample_with_replacement(max_n, sample_size, proba, num_samples=None):
     """
 
-    with ExitStack() as stack:
-        arrays = [stack.enter_context(shm.get_ndarray()) for shm in shm_arrays]
-    # with shm_array.get_ndarray() as array:
-        samps = [np.take(array, index, axis=axis) for array in arrays]
-    if estimator is not None:
-        # if there is *not*  an axis, then hope for the best
-        if 'axis' in get_default_args(estimator):
-            e_kwargs['axis'] = axis
-        return estimator(*samps, *e_args, **e_kwargs)
-    if len(samps) == 1:
-        samps = samps[0]
-    return samps
+    Parameters
+    ----------
+    max_n: int
+        maximum sample index to draw
+    sample_size: int
+        size of sample from range(0, max_n)
+    proba: ndarray
+        if given, the probability of drawing any index
+    num_samples: int
+        if given, generate this many permutations (otherwise generation is open-ended)
+
+    Returns
+    -------
+    Yields bootstrap-style resampling with replacement
+
+    """
+    r = np.arange(max_n)
+    i = 0
+    while True:
+        if proba is None:
+            yield np.random.randint(0, max_n, sample_size)
+        else:
+            yield np.random.choice(r, sample_size, p=proba)
+        i += 1
+        if num_samples is not None and i == num_samples:
+            return
+
+
+def leave_n_out_resample(n, r, max_samples, ordered_samples):
+    """
+
+    Parameters
+    ----------
+    n: int
+        original sample size
+    r: int
+        leave-n-out sample size
+    max_samples: int or None
+        limit leave-n-out combinations to a random draw of max_samples
+    ordered_samples: bool
+        order samples as (leave-0, leave-1, ...)
+
+    Returns
+    -------
+    Yields leave-n-out permutations
+
+    """
+    if max_samples is None:
+        resampler = combinations(range(n), r)
+        if ordered_samples:
+            resampler = list(resampler)[::-1]
+    else:
+        rng = range(n)
+        resampler = random_combinations(rng, r, max_samples)
+    for index in resampler:
+        yield index
+
+
+class BootstrapSampler(mp.Process):
+
+    def __init__(self, resamples: mp.JoinableQueue, results: mp.Queue, shm_arrays, shm_output, axis, estimator,
+                 e_args, e_kwargs, resample_args):
+        mp.Process.__init__(self)
+        # this is a JoinableQueue that the process will grab resampling permutations from
+        self.resamples = resamples
+        # this is a Queue that the the process will push results into
+        self.results = results
+        self.shm_arrays = shm_arrays
+        self.shm_output = shm_output
+        self.axis = axis
+        self.estimator = estimator
+        self.e_args = e_args
+        self.e_kwargs = e_kwargs
+        self.resample_args = resample_args
+
+    def sample_generator(self):
+        max_n, sample_size, proba = self.resample_args
+        for index in resample_with_replacement(max_n, sample_size, proba):
+            yield index
+
+    def _seed_rng(self):
+        time_parts = timestamp().split('-')
+        # sum HH-MM-SS
+        p1 = sum([int(p) for p in time_parts[:3]])
+        # multiply by other part
+        seed = p1 * int(time_parts[-1])
+        np.random.seed(seed)
+
+    def run(self):
+        info = mp.get_logger().info
+        shm_arrays = self.shm_arrays
+        shm_output = self.shm_output
+        axis = self.axis
+        estimator = self.estimator
+        e_args = self.e_args
+        e_kwargs = self.e_kwargs
+        sample_generator = self.sample_generator()
+        # be sure to start the RNG in a different state than peers
+        self._seed_rng()
+        info('Process running {}'.format(timestamp()))
+        while True:
+            job = self.resamples.get()
+            if job is None:
+                info('Caught exit signal: {}'.format(timestamp()))
+                self.resamples.task_done()
+                break
+            # this state is important for deterministic sampling (i.e. jackknife)
+            self.sample_num = job
+            permutation = next(sample_generator)
+
+            # This is the *safe* way to go: resample the arrays under a lock.
+            # It should also prevent making copies in each subprocess
+            if shm_output is None:
+                with ExitStack() as stack:
+                    arrays = [stack.enter_context(shm.get_ndarray()) for shm in shm_arrays]
+                    info('Sampling perm {} from arrays {}'.format(self.sample_num, timestamp()))
+                    samps = [np.take(array, permutation, axis=axis) for array in arrays]
+            else:
+                with ExitStack() as stack:
+                    arrays = [stack.enter_context(shm.get_ndarray()) for shm in shm_arrays]
+                    # de-reference the output memory that was allocated for this sample
+                    out_managers = shm_output[self.sample_num]
+                    out_arrays = [stack.enter_context(shm.get_ndarray()) for shm in out_managers]
+                    samps = [np.take(array, permutation, axis=axis, out=out)
+                             for (array, out) in zip(arrays, out_arrays)]
+            info('Doing perm {} estimator method {}'.format(self.sample_num, timestamp()))
+            if estimator is not None:
+                # if there is *not*  an axis, then hope for the best
+                if 'axis' in get_default_args(estimator):
+                    e_kwargs['axis'] = axis
+                r = estimator(*samps, *e_args, **e_kwargs)
+                info('**** Estimator {} done {} ****'.format(self.sample_num, timestamp()))
+                self.resamples.task_done()
+                self.results.put(r)
+            else:
+                # if we just filled pre-allocated memory, return the list index that was filled
+                self.resamples.task_done()
+                if shm_output is not None:
+                    self.results.put(self.sample_num)
+                else:
+                    # otherwise return the actual data
+                    if len(samps) == 1:
+                        samps = samps[0]
+                    self.results.put(samps)
+        info('Process ending {}'.format(timestamp()))
+
+
+class JackknifeSampler(BootstrapSampler):
+
+    def sample_generator(self):
+        # since resampling is deterministic for the Jackknife, need
+        # to create all permutations first and yield the one corresponding
+        # to the present job
+        n, n_out, max_samples, ordered_samples = self.resample_args
+        all_permutations = list(leave_n_out_resample(n, n_out, max_samples, ordered_samples))
+        while True:
+            yield all_permutations[self.sample_num]
 
 
 class Bootstrap:
@@ -65,7 +195,8 @@ class Bootstrap:
     Bootstrap resampler
     """
 
-    def __init__(self, arrays, num_samples, axis=-1, proba=None, sample_size=None, n_jobs=None, ordered_samples=False):
+    def __init__(self, arrays, num_samples, axis=-1, proba=None, sample_size=None, n_jobs=1, ordered_samples=False,
+                 subprocess_logging='error', alloc_shared=True):
         """
         Make a bootstrap resampler for an array.
 
@@ -86,6 +217,8 @@ class Bootstrap:
             Number of parallel jobs to make bootstrap samples. None uses cpu_count().
         ordered_samples: bool
             ???
+        subprocess_logging: str
+            Logging level (e.g. "error", "info", ...)
 
         """
 
@@ -106,40 +239,77 @@ class Bootstrap:
         else:
             self._sample_size = sample_size
         self._resampler = None
-        if sys.platform == 'win32':
-            self._n_jobs = 1
-        else:
-            self._n_jobs = n_jobs
         self._ordered_samples = ordered_samples
+        self._n_jobs = n_jobs
+        self._loglevel = subprocess_logging
+        self._alloc_shared = alloc_shared
 
     def _init_sampler(self):
         max_n = self._arrays[0].shape[self._axis]
         samp_size = self._sample_size
-        # use simple and faster randint if drawing uniform
-        if self._proba is None:
-            def anon_sampler():
-                for i in range(self._num_samples):
-                    yield np.random.randint(0, max_n, samp_size)
-        else:
-            def anon_sampler():
-                r = np.arange(max_n)
-                p = self._proba
-                for i in range(self._num_samples):
-                    yield np.random.choice(r, samp_size, p=p)
-        self._resampler = anon_sampler()
+        return resample_with_replacement(max_n, samp_size, self._proba, num_samples=self._num_samples)
+
+    def _alloc_output_memory(self):
+        # each sample will have n arrays with these shapes
+        output_shapes = [list(a.shape) for a in self._arrays]
+        output_types = [a.dtype.char for a in self._arrays]
+        for s in output_shapes:
+            s[self._axis] = self._sample_size
+        # create len(self) times these arrays in shared memory
+        shm_output = []
+        output_managers = []
+        for _ in range(len(self)):
+            arrays = [SharedmemManager.shared_ndarray(s, t) for (s, t) in zip(output_shapes, output_types)]
+            managers = [SharedmemManager(a) for a in arrays]
+            shm_output.append(arrays)
+            output_managers.append(managers)
+        return shm_output, output_managers
 
     def __len__(self):
         return self._num_samples
+
+    def _sampling_processes(self, task_queue, results_queue, estimator, e_args, e_kwargs):
+        """
+        Create the correct resampling subprocess.
+
+        Parameters
+        ----------
+        task_queue
+        results_queue
+        estimator
+        e_args
+        e_kwargs
+
+        Returns
+        -------
+
+        """
+        shm_arrays = [SharedmemManager(arr, use_lock=True) for arr in self._arrays]
+        if estimator is None and self._alloc_shared:
+            # if generating samples avoid heavy data passing -- pre-allocate shared memory
+            print('allocating output memory {}'.format(timestamp()), end='... ')
+            output_arrays, output_managers = self._alloc_output_memory()
+            print('done {}'.format(timestamp()))
+        else:
+            print('no output allocation')
+            output_arrays = output_managers = None
+
+        resample_args = (self._arrays[0].shape[self._axis], self._sample_size, self._proba)
+        samplers = []
+        for s in range(self._n_jobs):
+            s = BootstrapSampler(task_queue, results_queue, shm_arrays, output_managers, self._axis,
+                                 estimator, e_args, e_kwargs, resample_args)
+            samplers.append(s)
+        return samplers, output_arrays
 
     def sample(self, estimator=None, e_args=(), **e_kwargs):
         """Generate bootstrap samples, or estimates from the samples. The estimator must be a
         callable that accepts the "axis" keyword.
         """
 
-        self._init_sampler()
-        parallel = self._n_jobs is None or self._n_jobs > 1
+        parallel = self._n_jobs > 1
         if not parallel:
-            for samp_idx in self._resampler:
+            for samp_idx in self._init_sampler():
                 samps = [np.take(arr, samp_idx, axis=self._axis) for arr in self._arrays]
                 if estimator is not None:
                     if 'axis' in get_default_args(estimator):
@@ -150,19 +320,27 @@ class Bootstrap:
                         samps = samps[0]
                     yield samps
             return
-        shm_arrays = [SharedmemManager(arr, use_lock=True) for arr in self._arrays]
-        pool_args = dict(shm_arrays=shm_arrays,
-                         axis=self._axis, estimator=estimator,
-                         e_args=e_args, e_kwargs=e_kwargs)
-        with closing(mp.Pool(processes=self._n_jobs,
-                             initializer=_init_pool,
-                             initargs=(pool_args,))) as p:
-            if self._ordered_samples:
-                for samp in p.imap(_resampler, self._resampler):
-                    yield samp
-            else:
-                for samp in p.imap_unordered(_resampler, self._resampler):
-                    yield samp
+        # If parallel, build the workers, start the workers, push tasks, and collect results
+        permutations = mp.JoinableQueue()
+        resample_results = mp.Queue()
+        samplers, output_arrays = self._sampling_processes(permutations, resample_results, estimator, e_args, e_kwargs)
+        with mp.make_stderr_logger(self._loglevel):
+            for s in samplers:
+                s.start()
+            # put jobs into the queue (just the sample number)
+            for s_num in range(self._num_samples):
+                permutations.put(s_num)
+            # put the stop signal
+            for n in range(len(samplers)):
+                permutations.put(None)
+            # wait for the results
+            permutations.join()
+            # yield results
+            for n in range(self._num_samples):
+                r = resample_results.get()
+                if output_arrays is not None:
+                    r = output_arrays[r]
+                yield r
 
     def all_samples(self, estimator=None, e_args=(), **e_kwargs):
         """Return all samples from the generator"""
@@ -179,7 +357,7 @@ class Bootstrap:
         return np.mean(vals, axis=0)
 
     @classmethod
-    def bootstrap_estimate(cls, sample, num_resample, estimator, axis=-1, n_jobs=None, e_args=(), **e_kwargs):
+    def bootstrap_estimate(cls, sample, num_resample, estimator, axis=-1, n_jobs=1, e_args=(), **e_kwargs):
         bootstrapper = Bootstrap(sample, num_resample, axis=axis, n_jobs=n_jobs)
         return bootstrapper.estimate(estimator, e_args=e_args, **e_kwargs)
 
@@ -203,27 +381,60 @@ class Jackknife(Bootstrap):
 
     """
 
-    def __init__(self, arrays, n_out=1, axis=-1, max_samps=-1, n_jobs=None, ordered_samples=False):
+    def __init__(self, arrays, n_out=1, axis=-1, max_samps=None, n_jobs=None, ordered_samples=False):
         if isinstance(arrays, np.ndarray):
             N = arrays.shape[axis]
         else:
             N = arrays[0].shape[axis]
-        if max_samps > 0:
+        if max_samps is not None:
             num_samples = max_samps
         else:
             r = N - n_out
-            num_samples = comb(N, r)
+            num_samples = int(comb(N, r))
         super(Jackknife, self).__init__(arrays, num_samples, axis=axis, n_jobs=n_jobs, ordered_samples=ordered_samples)
-        self.__choose = (range(N), N - n_out)
+        # self.__choose = (range(N), N - n_out)
+        self._n_out = n_out
         self._max_samps = max_samps
 
+    def _sampling_processes(self, task_queue, results_queue, estimator, e_args, e_kwargs):
+        """
+        Create the correct resampling subprocess.
+
+        Parameters
+        ----------
+        task_queue
+        results_queue
+        estimator
+        e_args
+        e_kwargs
+
+        Returns
+        -------
+
+        """
+        shm_arrays = [SharedmemManager(arr, use_lock=True) for arr in self._arrays]
+        if estimator is None:
+            # if generating samples avoid heavy data passing -- pre-allocate shared memory
+            print('allocating output memory')
+            output_arrays, output_managers = self._alloc_output_memory()
+        else:
+            print('no output allocation')
+            output_arrays = output_managers = None
+
+        n = self._arrays[0].shape[self._axis]
+        r = n - self._n_out
+        resample_args = (n, r, self._max_samps, self._ordered_samples)
+        samplers = []
+        for s in range(self._n_jobs):
+            s = JackknifeSampler(task_queue, results_queue, shm_arrays, output_managers, self._axis,
+                                 estimator, e_args, e_kwargs, resample_args)
+            samplers.append(s)
+        return samplers, output_arrays
+
     def _init_sampler(self):
-        if self._max_samps < 0:
-            self._resampler = list(combinations(*self.__choose))[::-1]
-        elif self._resampler is None:
-            iterable, r = self.__choose
-            self._resampler = random_combinations(iterable, r, self._max_samps)
-        # else random sampler is already set
+        n = self._arrays[0].shape[self._axis]
+        r = n - self._n_out
+        return leave_n_out_resample(n, r, self._max_samps, self._ordered_samples)
 
     def pseudovals(self, estimator, jn_samples=(), e_args=(), **e_kwargs):
         """Return the bias-correcting pseudovalues of the estimator"""
@@ -234,7 +445,7 @@ class Jackknife(Bootstrap):
             e_kwargs['axis'] = self._axis
         theta = estimator(*self._arrays, *e_args, **e_kwargs)
         N1 = float(self._arrays[0].shape[self._axis])
-        d = N1 - self.__choose[1]
+        d = self._n_out
         return N1 / d * theta - (N1 - d) * jn_samples / d
 
     def estimate(self, estimator, correct_bias=True, se=False, e_args=(), **e_kwargs):
@@ -279,6 +490,6 @@ class Jackknife(Bootstrap):
 
     @classmethod
     def jackknife_estimate(cls, sample, estimator, correct_bias=True, se=False,
-                           axis=-1, n_jobs=None, e_args=(), **e_kwargs):
-        sampler = Jackknife(sample, axis=axis, n_jobs=n_jobs)
+                           axis=-1, max_samps=None, n_jobs=None, e_args=(), **e_kwargs):
+        sampler = Jackknife(sample, axis=axis, max_samps=max_samps, n_jobs=n_jobs)
         return sampler.estimate(estimator, correct_bias=correct_bias, se=se, e_args=e_args, **e_kwargs)
