b38cd7d3a3025bf785e580a1d8fed87e0768025c
miketrumpis <mtrumpis@gmail.com>
2021-11-03 19:13:13 -0700

    update to pep518 standard pip install + tox testing

---
+++ ecoglib/__init__.py
@@ -0,0 +1,7 @@
+from . import estimation
+from . import graph
+from . import signal_testing
+from . import vis
+
+
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
-import os
-from setuptools import setup, Extension, find_packages
-from numpy.distutils.command import build_src
-import Cython.Compiler.Main
-build_src.Pyrex = Cython
-build_src.have_pyrex = True
-from Cython.Distutils import build_ext
+#!/usr/bin/env python
+from setuptools import setup, Extension
 import numpy
+
 
 try:
     from numpy.distutils.misc_util import get_numpy_include_dirs
@@ -14,48 +10,31 @@ except AttributeError:
     numpy_include_dirs = numpy.get_include()
 
 
-dirs = list(numpy_include_dirs)
-
-cknn_graph = Extension(
-    'ecoglib.graph.cknn_graph',
-    ['ecoglib/graph/cknn_graph.pyx'], 
-    include_dirs = dirs,
-    extra_compile_args=['-O3']
-    )
-
-bispectrum = Extension(
-    'ecoglib.estimation._bispectrum',
-    ['ecoglib/estimation/_bispectrum.pyx'], 
-    include_dirs = dirs, 
-    extra_compile_args=['-O3']
-    )
-
-semivariance = Extension(
-    'ecoglib.estimation.spatial_variance._semivariance',
-    ['ecoglib/estimation/spatial_variance/_semivariance.pyx'],
-    include_dirs = dirs, 
-    extra_compile_args=['-O3']
-    )
-
-with open('requirements.txt') as f:
-    reqs = f.readlines()
-    reqs = list(map(str.strip, reqs))
-    def _del_soft_reqs(s):
-        if 'mayavi' in s.lower():
-            return False
-        if 'pyside' in s.lower():
-            return False
-        return True
-    reqs = list(filter(_del_soft_reqs, reqs))
+header_dirs = list(numpy_include_dirs)
 
 if __name__=='__main__':
+    cknn_graph = Extension(
+        'ecoglib.graph.cknn_graph',
+        ['ecoglib/graph/cknn_graph.pyx'],
+        include_dirs=header_dirs,
+        extra_compile_args=['-O3']
+    )
+
+    bispectrum = Extension(
+        'ecoglib.estimation._bispectrum',
+        ['ecoglib/estimation/_bispectrum.pyx'],
+        include_dirs=header_dirs,
+        extra_compile_args=['-O3']
+    )
+
+    semivariance = Extension(
+        'ecoglib.estimation.spatial_variance._semivariance',
+        ['ecoglib/estimation/spatial_variance/_semivariance.pyx'],
+        include_dirs=header_dirs,
+        extra_compile_args=['-O3']
+    )
     setup(
-        name = 'ecoglib',
-        version = '0.1',
-        packages = find_packages(exclude=['demos', 'notes_n_demos', 'build']),
         ext_modules = [cknn_graph,
                        bispectrum,
                        semivariance],
-        cmdclass = {'build_ext': build_ext},
-        install_requires=reqs
     )
