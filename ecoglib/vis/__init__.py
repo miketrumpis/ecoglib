311e08483dc2b651565715eafb1e7073e4e20b34
miketrumpis <mtrumpis@gmail.com>
2022-01-11 15:46:44 -0800

    include another jupyter runtime backedn

--- ecoglib/vis/__init__.py
+++ ecoglib/vis/__init__.py
@@ -46,7 +46,7 @@ class _plot_libraries:
     def jupyter_runtime(self):
         be = self.mpl.get_backend()
         # This likely can be simplified to "backend_inline" in mpl.get_backend()
-        if 'ipykernel.pylab.backend_inline' in be or 'matplotlib_inline.backend_inline' in be:
+        if 'ipykernel.pylab.backend_inline' in be or 'matplotlib_inline.backend_inline' in be or 'ipympl' in be:
             return True
         return False
 
