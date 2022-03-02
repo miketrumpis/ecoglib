2ecc5bc64920d96e01297cce5472d4b4797c3a7d
miketrumpis <mtrumpis@gmail.com>
2022-03-01 23:06:06 -0800

    prevent channel picker import unless you really want it

--- ecoglib/signal_testing/__init__.py
+++ ecoglib/signal_testing/__init__.py
@@ -1,6 +1,10 @@
 from .signal_tools import *
 from .signal_plots import *
 from ..vis import plotters
-# if running jupyter, avoid importing traitsui stuff (which will launch a qt app)
-if not plotters.jupyter_runtime:
+# if runing jupyter, avoid importing traitsui stuff (which will launch a qt app)
+# TODO: STILL NEED A THREE-WAY DISTINCTION:
+#   1) JUPYTER RUNTIME (OBS NO TRAITSUI)
+#   2 & 3) TRUE HEADLESS VERSUS AGG BACKEND IN GUI MODE
+#  I guess if the GUI app sets MPL to Agg, then it can be responsible for explicitly importing chanenl picker
+if not (plotters.headless or plotters.jupyter_runtime):
     from .channel_picker import *
